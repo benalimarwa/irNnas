f09b1a88504f5a4a62271df9b66440cd55b30389
marwa <marwa.benali2003@gmail.com>
2026-06-23 00:41:38 +0100

    premier commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,8 @@
-// app/layout.tsx
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-import Navbar from "@/components/AdminNavbar";
 import { frFR } from '@clerk/localizations';
-import ClientNavbar from "@/components/ClientNavbar";
-import AdminNavbar from "@/components/AdminNavbar";
 import SyncUser from "@/components/SyncUser";
+
 export const metadata = {
   title: "IRNAS",
   description: "Boutique de luxe en Tunisie",
@@ -17,10 +14,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-     <ClerkProvider localization={frFR}>
+    <ClerkProvider localization={frFR}>
       <html lang="fr">
-        <body className="overflow-x-hidden w-full max-w-full">
-         <SyncUser />
+        <head>
+          {/* ✅ CRUCIAL — sans ça le mobile affiche en mode desktop */}
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </head>
+        <body className="overflow-x-hidden w-full max-w-full min-h-screen">
+          <SyncUser />
           <main className="pt-20 bg-black">{children}</main>
         </body>
       </html>
