8688546d87708e68d13d13b596ca8aed688b8a43
marwa <marwa.benali2003@gmail.com>
2026-06-26 17:24:39 +0100

    premier commit

--- app/client/page.tsx
+++ app/client/page.tsx
@@ -148,7 +148,7 @@ export default function ClientDashboard() {
           }}
           onError={(e) => { e.currentTarget.style.display = "none"; }}
         >
-          <source src="/video/mm.mp4" type="video/mp4" />
+          <source src="/video/pp.mp4" type="video/mp4" />
         </video>
       </div>
 
